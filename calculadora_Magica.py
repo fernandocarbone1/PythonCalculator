65b237b788cb3b13b2451f1ba144b9cb9b3fbdae
Fernando Carboné <fernando.carbone707@gmail.com>
2022-09-22 22:50:00 -0300

    Complexing code

--- calculadora_Magica.py
+++ calculadora_Magica.py
@@ -9,11 +9,23 @@ run = True
 def performMath():
     global run
     global previous
-    equation = input("Enter equation: ")
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation == input(str(previous))
+
+
     if equation == "quit":
         run = False
     else:
-        previous = eval(equation)
+        equation == re.sub('[a-zA-Z,.:()" "]','', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
         print("You typed", previous)
 
 while run:
